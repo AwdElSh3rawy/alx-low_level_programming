43f575f7f1a67128e1dafcb16f42fc0bedcab484
AwdElSh3rawy <117772832+AwdElSh3rawy@users.noreply.github.com>
2022-12-08 15:24:40 +0200

    Mandatory Tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@
 
 int main(void)
 {
-
 	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
