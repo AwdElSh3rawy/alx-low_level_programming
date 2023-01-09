4d1a7219f00b94f63c89b7c84dfebd8d708c208d
AwdElSh3rawy <117772832+AwdElSh3rawy@users.noreply.github.com>
2023-01-09 14:01:57 +0200

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,70 +1,49 @@
 #include <stdlib.h>
-#include <string.h>
-#include "main.h"
-
-/* char *str_reduct(char **str, int i); */
-/* int _strlen(char *s); */
 
 /**
- * argstostr - concatenates all the arguments of a program
- * @ac: number of arguments
- * @av: array of pointer to the arguments
+ * argstostr - a function that concatenates
+ *             all the arguments
  *
- * Return: a pointer to a new string or NULL if it fails
- */
+ * @ac: argument counter
+ * @av: argument holder
+ *
+ * Return: a pointer to a new string
+ *         or NULL if it fails
+*/
+
 char *argstostr(int ac, char **av)
 {
-	char *arr, *array;
-	int i, j;
+	int i, j, k, length;
+	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
+	/*find length of vector + '\0' which makes it a 2d array*/
+	length = 0;
 	for (i = 0; i < ac; i++)
 	{
-		array = malloc(strlen(av[i]) + 1);
-		if (array == 0)
-			return (NULL);
-		for (j = 0; j < _strlen(av[i]) + 1; j++)
-			array[j] = av[i][j];
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+		length++;
 	}
-	return (0);
+
+	str = malloc((length + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
 }
-
-/**
- *  * _strlen_recursion - returns the length of a string
- *   * @s: string to calculate the length
- *    *
- *     * Return: the length of the string (int)
- *      */
-int _strlen(char *s)
-{
-	        int length;
-
-		        length = 0;
-			        if (s[0] == '\0')
-					                length = 0;
-				        else if (s[0] == '\0')
-						                length = 1;
-					        else
-							                length = _strlen(s + 1) + 1;
-						        return (length);
-}
-
-
-/**
- * str_reduct - reduce the string
- * @str: pointer to a pointer
- *
- * Return: un string
- *
-char *str_reduct(char **str, int i)
-{
-	char *reduct;
-	int j;
-
-	if (strlen(str[i]) == 0)
-		reduct = ' ';
-	for (j = 0; j < strlen(str[i]); j++)
-		reduct[j] = str[i][j];
-	return (reduct);
-}*/
